6615e28d9a9246b81aaaa01d6cec88c80d48619f
Ian <shameless@galage.cn>
2022-12-21 14:21:33 +0800

    项目搭建

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Button } from 'antd-mobile'
 function App() {
-  return <div>111</div>
+  return (
+    <div>
+      111
+      <Button color="primary" fill="solid">
+        None
+      </Button>
+    </div>
+  )
 }
 
 export default App
