7796058b8b65f0b95a20d690f47d5926ce9825fd
Ian <shameless@galage.cn>
2022-12-22 15:21:41 +0800

    登录功能axios封装

--- src/component/Input/index.js
+++ src/component/Input/index.js
@@ -1,9 +1,10 @@
 import styles from './index.module.scss'
-export default function Input({ extra, onExtraClick, ...rest }) {
+import classNames from 'classname'
+export default function Input({ extra, onExtraClick, className, ...rest }) {
   return (
     <div>
       <div className={styles.root}>
-        <input className="input" {...rest}></input>
+        <input className={classNames('input', className)} {...rest}></input>
         {extra && (
           <div className="extra" onClick={onExtraClick}>
             {extra}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,78 @@
 import Navbar from '../../component/Navbar'
 import styles from './index.module.scss'
 import Input from '../../component/Input/index'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import classNames from 'classname'
 function Login() {
   const onExtraClick = () => {}
+  const formik = useFormik({
+    initialValues: {
+      mobile: '',
+      code: '',
+    },
+    onSubmit(values) {
+      console.log(values)
+    },
+    validationSchema: Yup.object({
+      mobile: Yup.string()
+        .required('手机号不能为空')
+        .matches(/^1[3-9]\d{9}$/, '格式错误'),
+      code: Yup.string()
+        .required('验证码不为空')
+        .matches(/^\d{6}$/, '验证码错误'),
+    }),
+  })
+  const {
+    values: { mobile, code },
+    handleChange,
+    handleSubmit,
+    errors,
+    handleBlur,
+    touched,
+    isValid,
+  } = formik
   return (
     <div className={styles.root}>
       <Navbar>登录</Navbar>
       <div className="content">
         <h3>短信登录</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-item">
-            <Input placeholder="请输入手机号"></Input>
-            <div className="validate">手机号验证错误</div>
+            <Input
+              placeholder="请输入手机号"
+              value={mobile}
+              onChange={handleChange}
+              name="mobile"
+              autoComplete="off"
+              onBlur={handleBlur}
+              maxLength={11}
+            ></Input>
+            {touched.mobile && errors.mobile ? (
+              <div className="validate">{errors.mobile}</div>
+            ) : null}
           </div>
           <div className="input-item">
             <Input
               placeholder="请输入验证码"
               extra="获取验证码"
               onExtraClick={onExtraClick}
+              value={code}
+              onChange={handleChange}
+              name="code"
+              autoComplete="off"
+              onBlur={handleBlur}
+              maxLength={6}
             ></Input>
-            <div className="validate">验证码验证错误</div>
+            {touched.code && errors.code ? (
+              <div className="validate">{errors.code}</div>
+            ) : null}
           </div>
-          <button type="submit" className="login-btn">
+          <button
+            type="submit"
+            className={classNames('login-btn', { disabled: !isValid })}
+            disabled={!isValid}
+          >
             登录
           </button>
         </form>
